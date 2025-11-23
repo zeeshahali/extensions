8c8ca2ef2bfed45afd9efa2b1f6970715677fd50
Zeeshah Butt <z3eshahali@gmail.com>
2025-11-23 20:41:17 +0500

    Update nyaasi.js

--- nyaasi.js
+++ nyaasi.js
@@ -7,6 +7,7 @@ export default new class NyaaSi extends AbstractSource {
   async single({ titles, episode }) {
     if (!titles?.length) return []
 
+    throw new Error(titles)
     const query = this.buildQuery(titles[0], episode)
     const url = `${this.base}${encodeURIComponent(query)}`
 
